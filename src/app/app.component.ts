f465e291497971ed3731428c732e133d0dd6af52
IdrissEl <idrisselmellaoui@outlook.com>
2022-04-22 15:16:01 +0200

    firts commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './products.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Stage';
+  productTitle:string = 'new product';
+  productPrice:number = 5;
+  productSale:number = 7;
+
+  products: Product[] = [
+    new Product('test', 1, 0 ),
+    new Product('test', 1, 0)
+  ]
+
+  public addProducts() {
+    console.log('test')
+    this.products.push(
+      new Product(this.productTitle, this.productPrice, this.productSale)
+    )
+  }
 }
